004a6228dadc51f19dc6c7b4a48c3f38a88f1066
Isabella <isasabo08@gmail.com>
2026-02-23 22:35:58 -0300

    Create primeiro_programa.py

---
+++ primeiro_programa.py
@@ -0,0 +1,7 @@
+# Meu Primeiro Projeto
+
+Olá! Meu nome é Isabella Sabo.
+
+Sou estudante de Engenharia de Software e estou começando minha jornada na programação.
+
+Este repositório faz parte do meu aprendizado em Python.
